c4171a4dd4a8875e692f019bc049633df39cc177
code-w0lf <vuki.stanojevic@protonmail.com>
2021-08-26 23:10:09 +1200

    Add boostrap-react and simple layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap';
+import Box from './Box';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid className='bg-white text-center p-5' style={{height: '100%'}}>
+      <h1 className="mb-5">TEN TEN</h1>
+      <Box></Box>
+    </Container>
   );
 }
 
